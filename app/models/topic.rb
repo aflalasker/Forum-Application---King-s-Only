5382c5351e26f3e2c31fcbe8b48b5231bf85971e
Mohamed Aflal Mohamed Jafferdeen Mohamed Asker <mohamed.mohamed_jafferdeen_mohamed_asker@kcl.ac.uk>
2017-11-30 16:55:47 +0000

    Fix #3 Add Stylesheets for the views

---
+++ app/models/topic.rb
@@ -0,0 +1,28 @@
+class Topic < ActiveRecord::Base
+    belongs_to :category 
+    has_many :posts
+    
+    before_validation :add_default_permalink
+    after_save :touch_post
+    after_destroy :delete_related_categories
+    
+    validates_presence_of :name
+    validates_presence_of :name, :maximum => 255
+    
+    scope :newest_first, lambda { order("topics.created_at DESC") }
+    
+    private
+        def add_default_permalink
+            self.permalink = "#{id}-#{name.parameterize}"
+        end
+        
+        def touch_post
+            post.touch 
+        end
+        
+        def delete_related_posts 
+            self.posts.each do |post|
+                post.destroy
+            end
+        end
+end
