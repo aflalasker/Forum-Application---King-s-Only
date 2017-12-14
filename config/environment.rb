82650b984d19a469f6b41ec5a56eb8ac8a0f9bee
Jakub Uher <jakub.uher@kcl.ac.uk>
2017-12-14 17:55:34 +0000

    Fix #45
    Finalise design elements of the forum.

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,14 @@ require File.expand_path('../application', __FILE__)
 # ENV['CLIENT_ID'] = '85f1ef61-f210-4c18-afbf-9fcfb8093a73'
 # ENV['CLIENT_SECRET'] = 'kfnlwCUORLO21)-^-czG040'
 
-ENV['CLIENT_ID'] = '3978b7b5-5f45-4b68-a466-639d49b6e002'
-ENV['CLIENT_SECRET'] = 'znePVVDGD286@rhbaW97*:('
+# alin data
+
+# ENV['CLIENT_ID'] = '3978b7b5-5f45-4b68-a466-639d49b6e002'
+# ENV['CLIENT_SECRET'] = 'znePVVDGD286@rhbaW97*:('
+
+# jakub data
+ENV['CLIENT_ID'] = 'f433758e-e631-4fa2-aa1f-6c1760107228'
+ENV['CLIENT_SECRET'] = 'svalKWUEGO860:~[jvxG57+'
 
 ENV['OAUTH_SCOPE'] =
   'openid ' \
