c2afe9bdecd3e3aa5118638e23c947cf8a025621
Jack Hinshelwood <jack.hinshelwood@kcl.ac.uk>
2017-12-14 19:17:54 +0000

    further tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -16,4 +16,14 @@ class PostTest < ActiveSupport::TestCase
     post = Post.new(topic_id: '1')
     assert_not_nil post.valid?
   end
+  
+  test 'post has votes if they are assigned to it' do
+        post = Post.new(content: 'content', topic_id: '1')
+        post.save
+        user = User.new(first_name: 'Alin', last_name: 'Fulga', email: 'a.f@kcl.ac.uk')
+        user.save
+        vote = Vote.new(user_id: user.id, post_id: post.id)
+        vote.save
+        assert post.votes.include?(vote)
+   end
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,11 +1,17 @@
 require "test_helper"
 
 class VoteTest < ActiveSupport::TestCase
-  def vote
-    @vote ||= Vote.new
-  end
+  #def vote
+  #  @vote ||= Vote.new
+  #end
 
-  def test_valid
-    assert vote.valid?
+  #def test_valid
+   # assert vote.valid?
+  #end
+  
+  
+  test 'valid vote' do
+    vote = Vote.new(first_name: 'Alin', last_name: 'Fulga', email: 'alin.fulga@kcl.ac.uk')
+    assert user.valid?
   end
 end
