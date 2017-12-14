d71074226feab7c213b593d0edd5702b4a0da370
Alin Fulga <alin.fulga@kcl.ac.uk>
2017-12-14 01:33:30 +0000

    Fix #54
    Add voting system for posts into the application.

---
+++ db/migrate/20171214000836_create_votes.rb
@@ -0,0 +1,11 @@
+class CreateVotes < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.integer 'post_id'
+      t.integer 'user_id'
+      t.integer 'score'
+      t.timestamps null: false
+    end
+    add_index 'votes', ['post_id', 'user_id'], :unique => true
+  end
+end
